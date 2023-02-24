7f55ce2d77ad152a0a9ac52b35e8f839b97d9cb1
cs1807cwy <43921644+cs1807cwy@users.noreply.github.com>
2023-02-24 16:55:03 +0800

    Test train on single GPU, currently error with DDP.

--- GenerativeInpaintingTask/Train.py
+++ GenerativeInpaintingTask/Train.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer
+from pytorch_lightning.strategies.ddp import DDPStrategy
 from pytorch_lightning.callbacks.progress import TQDMProgressBar
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger, CSVLogger
@@ -19,10 +20,11 @@ batch_size = 4
 
 
 def train():
-    dm = ILSVRC2012_Task3(out_shape=(256, 256), batch_size=batch_size)
+    data_module = ILSVRC2012_Task3(out_shape=(256, 256), batch_size=batch_size, num_workers=4)
     model = SNPatchGAN(256, 256, 3, 128, 128, 32, 32, 0, 0, batch_size=batch_size)
-    tensorboard = TensorBoardLogger(save_dir='.', name='SN_PatchGAN_logs')
+    # tensorboard = TensorBoardLogger(save_dir='.', name='SN_PatchGAN_logs')
     csv = CSVLogger(save_dir='.', name='SN_PatchGAN_logs')
+    ddp_strategy = DDPStrategy(find_unused_parameters=False)
     checkpoint_callback = ModelCheckpoint(
         save_last=True,
         every_n_epochs=train_save_point_epoches,
@@ -33,15 +35,16 @@ def train():
         default_root_dir='./SN_PatchGAN_logs',
         accelerator="gpu",
         devices=[1, 7] if torch.cuda.is_available() else None,  # limiting got iPython runs
-        max_epochs=10,
-        max_steps=max_iteration,
+        # max_epochs=10,
+        max_steps=150,
         callbacks=[TQDMProgressBar(refresh_rate=20), checkpoint_callback],
-        strategy='ddp',
+        strategy=ddp_strategy,
         check_val_every_n_epoch=None,
         val_check_interval=validation_period_step,
-        limit_val_batches=1. if validation else 0
+        limit_val_batches=1. if validation else 0,
+        log_every_n_steps=1,
     )
-    trainer.fit(model, dm)
+    trainer.fit(model, data_module)
 
 
 if __name__ == '__main__':
